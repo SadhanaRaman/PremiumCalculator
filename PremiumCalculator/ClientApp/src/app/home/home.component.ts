1600b4bf76f7f24f808dec88a74a3f89542e9090
Sadhana Kalyana Raman <Sadhu4590@gmail.com>
2021-05-30 04:50:57 +1000

    Adding model binding, calling the API post method from the form

--- PremiumCalculator/ClientApp/src/app/home/home.component.ts
+++ PremiumCalculator/ClientApp/src/app/home/home.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
   public currentCount = 0;
+  public premium: Premium;
 
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {}
+  //constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  //  http.get<Premium>(baseUrl + 'premium').subscribe(result => {
+  //    this.premium = result;
+  //  }, error => console.error(error));
+  //}
   public incrementCounter() {
     this.currentCount++;
   }
+ // http://localhost:49483/
+  public onSubmit(data) {
+    this.http.post("http://localhost:49483/" + 'api/premium', data).subscribe(result => { console.warn("result", result) })
+    console.warn(data)
+  }
+}
+
+interface Premium {
+  premium: number;
 }
