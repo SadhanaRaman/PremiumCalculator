fe375a5e234c6d68030f8c1cc2842381d7fa4600
Sadhana Kalyana Raman <Sadhu4590@gmail.com>
2021-05-29 22:58:50 +1000

    Update the Controller to return the premium for the set of input parameters

--- PremiumCalculator/Controllers/PremiumController.cs
+++ PremiumCalculator/Controllers/PremiumController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PremiumCalculator.Services.Interfaces;
 
 namespace PremiumCalculator.Controllers
 {
@@ -11,6 +12,28 @@ namespace PremiumCalculator.Controllers
     [ApiController]
     public class PremiumController : ControllerBase
     {
-
+        private readonly IPremiumService _premiumService;
+        public PremiumController(IPremiumService premiumService)
+        {
+            _premiumService = premiumService;
+        }
+        /// <summary>
+        /// Calculates and Returns the Premium for a given set of Options
+        /// </summary>
+        /// <param name="cover"></param>
+        /// <param name="occupation"></param>
+        /// <param name="birthdate"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public double GetPremium(double cover, string occupation, DateTime birthdate)
+        {
+            //throw new NotImplementedException();
+            var premium = _premiumService.CalculatePremium(cover, occupation, birthdate);
+            {
+                return premium;
+            }
+        }
     }
 }
